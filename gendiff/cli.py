7c40e7d481da2d875c668577db14236e4a2c99a1
SergSm <smirnovserg.s@gmail.com>
2020-10-18 19:52:14 +0600

    Step 2 - gendiff -h -f

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -4,8 +4,12 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Generate diff")
 
+    # positional arguments
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
+    # optional arguments shor and long forms
+    parser.add_argument('-f', '--format', help="set format of output")
+
+
     args = parser.parse_args()
-    print(args.echo)
