294c404a5de203bb0e09337742586030b12925d1
SergSm <smirnovserg.s@gmail.com>
2021-01-26 09:55:08 +0600

    bit of flake8

--- gendiff/main.py
+++ gendiff/main.py
@@ -9,18 +9,15 @@ ACCEPTABLE_INPUT_EXTENSIONS = ('.JSON', '.YML', '.YAML')
 ACCEPTABLE_OUTPUT_FORMATTERS = ('stylish',)
 
 
-def generate_diff(filepath1, filepath2, output_format): #
+def generate_diff(filepath1, filepath2, output_format):
     """the main function of the library"""
 
     file_ext1 = get_file_extension(filepath1).upper()
     file_ext2 = get_file_extension(filepath2).upper()
 
-
-
     if not (file_ext1, file_ext2 in ACCEPTABLE_INPUT_EXTENSIONS):
         raise ValueError(f'Unknown file extension "{file_ext1}",'
                          f' "{file_ext2}" or file(s) has no extension(s)')
-
 
     file1 = get_file_object(filepath1)
     file2 = get_file_object(filepath2)
