f0afaeb4c9629225dff6f19acc64350e81f2f845
SergSm <smirnovserg.s@gmail.com>
2021-02-01 13:58:55 +0600

    WIP

--- gendiff/difference_builder.py
+++ gendiff/difference_builder.py
@@ -1,41 +1,71 @@
-def get_difference(dict1, dict2):
-    """Return the difference between 2 dicts"""
+def build_dif(dict1, dict2):
+
+    result = []
 
     # convert to set to have only unique values
     dict1_keys = set(dict1.keys())
     dict2_keys = set(dict2.keys())
 
     shared_keys = sorted(dict1_keys.intersection(dict2_keys))
+    deleted_keys = sorted(dict1_keys - dict2_keys) # {-}
+    new_keys = sorted(dict2_keys - dict1_keys) # {+}
 
-    # {-}
-    keys_left = sorted(dict1_keys - dict2_keys)
+    # handle deleted keys
+    for key in deleted_keys:
+        row = {'key':key,
+               'value': dict1[key],
+               'state': 'DELETED'}
+        result.append(row)
 
-    # {+}
-    new_keys = sorted(dict2_keys - dict1_keys)
+    # handle new keys
+    for key in new_keys:
+        row = {'key': key,
+               'value': dict2[key],
+               'state': 'ADDED'}
+        result.append(row)
 
-    # {-} {+} or { }
-    unchanged, changed = set(), set()
-
+    # handle common keys
     for key in shared_keys:
+        ############################################################
+        # value1 == value2              | not dict                 #
+        # value1 != value2              | not dict changed -+      #
+        # value1 is dict                                           #
+        #       AND value 2 is dict     | call build_dif           #
+        # value1 is dict                                           #
+        #       AND value 2 is not dict | -+changed call build_diff#
+        # value1 is NOT dict                                       #
+        #       AND value 2 is dict     | -+changed call build_diff#
+        ############################################################
 
-        if type(dict1[key]) is dict\
+        if dict1[key] == dict2[key]:
+            row = {'key': key,
+                   'value': dict1[key],
+                   'state': 'UNCHANGED'
+                   }
+        # TODO: check if only one of them is a dict
+        elif type(dict1[key]) is dict\
                 or type(dict2[key]) is dict:
-            return get_difference(dict1[key], dict2[key])
+            row = {'key': key,
+                   'value': build_dif(dict1[key], dict2[key]),
+                   'state': 'PARENT',
+                   }
+        elif dict1[key] != dict2[key]:  # simple types, non-dictionary
+            row = {'key': key,
+                   'value_left': dict1[key],
+                   'value_right': dict2[key],
+                   'state': 'CHANGED'
+                   }
 
-        #if type(dict2[key]) is dict:
+        result.append(row)
+
+    return result
 
 
-        if dict1[key] == dict2[key]
+def get_difference(dict1, dict2):
+    """Return the difference between 2 dicts"""
 
-        if dict2[key] == dict1[key]:
-            unchanged.add(key)
-        else:
-            changed.add(key)
+    result = {'key': 'root',
+              'value': build_dif(dict1, dict2)
+              }
 
-    unchanged = sorted(unchanged)
-    changed = sorted(changed)
-
-    #
-    if val1 != val2 and val2 has children
-
-    return diff_tree  # DEBUG
+    return result
