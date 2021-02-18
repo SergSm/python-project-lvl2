34528b993b2b7e166014265d9cffe79115fd2d54
SergSm <smirnovserg.s@gmail.com>
2021-02-19 00:39:46 +0600

    WIP

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,132 +1,109 @@
 
-def get_spaces(number_of_spaces):
-    spaces = ""
-    for i in range(0, number_of_spaces):
-        #spaces += "\t"
-        spaces += " "
-    return spaces
-
-
-# DEBUG
-dct = {'group2': {'abc': '12345', 'deep': {'id': '45'}}}
-
 
 def get_mspaces(depth):
-    spaaaace = ''
-    for x in range(1, 4*depth):
-        spaaaace += ' '
-    return spaaaace
+    return ' ' * (depth * 4 - 2)
 
 
-def get_nicely_printed_dict(dictionary, depth):
-
-    astier = ''
-
-    count = 0
-    for x, y in dictionary.items():
-        count += 1
-
-        if type(y) is dict:
-
-            if not (depth == 0 and count == 1):
-                astier += '\n'
-
-            astier += get_mspaces(depth) \
-                      + x + ': {' \
-                      + '\n'
-
-            depth += 1
-
-            astier += get_nicely_printed_dict(y, depth) \
-                      + get_mspaces(depth) \
-                      + '\n' \
-                      + get_mspaces(depth) \
-                      + '}'
-        else:
-            astier += get_mspaces(depth) + str(x) + ': ' + str(y)
-            # if it is the last record in dict do not add newstring
-            if count < len(dct):
-                astier += '\n'
-
-    return astier
-
-
-#  try 3
-def get_formatted_dict(dictionary, nesting_level):
-
-    dict_str_result = ""
-    for x, y in dictionary.items():
-        if not type(y) is dict:
-            dict_str_result += f'{x}: {y}'
-        else:
-            dict_str_result += f'{x}: {get_formatted_dict(y)}'
-
-    return dict_str_result
+# def get_nicely_printed_data(data, depth):
+#
+#     # if d
+#
+#     astier = ''
+#
+#     count = 0
+#     for x, y in dictionary.items():
+#         count += 1
+#
+#         if type(y) is dict:
+#
+#             if not (depth == 0 and count == 1):
+#                 astier += '\n'
+#
+#             astier += get_mspaces(depth) \
+#                       + x \
+#                       + ': {' \
+#                       + '\n'
+#
+#             depth += 1
+#
+#             astier += get_nicely_printed_dict(y, depth) \
+#                       + get_mspaces(depth) \
+#                       + '\n' \
+#                       + get_mspaces(depth) \
+#                       + '}'
+#         else:
+#             astier += get_mspaces(depth) + str(x) + ': ' + str(y)
+#             # if it is the last record in dict do not add newstring
+#             if count < len(dictionary):
+#                 astier += '\n'
+#
+#     return astier
 
 
-def get_children(data, nesting_level):
+def render_data(data, nesting_level):
 
-    nesting_level += 1
+    handled_data = ''+get_mspaces(nesting_level) + '{' + '\n'
 
-    text_diff = '{'
+    if type(data) is list:
+        for record in data:
+            handled_data += render_data(record, nesting_level + 1)
+    elif type(data) is dict:
+        handled_data += get_children(data, nesting_level + 1)
 
-    for record in data:
-        spaces = get_spaces(nesting_level)  # for a visual indentation purpose
+    handled_data += get_mspaces(nesting_level) + handled_data + '\n' + '}'
 
-        ############################################################################
+    return handled_data
 
-        prefix = f'\n{spaces}'
 
-        if record['STATE'] == 'CHANGED':
+def get_children(record, nesting_level):
+
+    # for a visual indentation purpose
+    spaces = get_mspaces(nesting_level)
+    prefix = f'\n{spaces}'
+
+    text_diff = spaces + '{'
+    ##########################################
+
+    if 'STATE' in record.keys():
+        if not record['STATE'] == 'CHANGED':
+            if type(record["VALUE"]) is dict:
+                value_left = get_children(record["VALUE"], nesting_level + 1)
+            elif type(record["VALUE"]) is list:
+                value_left = render_data(record["VALUE"], nesting_level)
+            else:
+                value_left = record["VALUE"]
+        elif record['STATE'] == 'CHANGED':
             value_left = record["VALUE_LEFT"]
             value_right = record["VALUE_RIGHT"]
+            if type(value_left) is dict:
+                value_left = get_children(record["VALUE_LEFT"], nesting_level + 1)
         else:
             value_left = record["VALUE"]
-            if type(value_left) is dict:
-                #value_left = get_formatted_dict(value_left)
-                pass
-                #value_left = '{\n' \
-                 #           git  + get_nicely_printed_dict(value_left, nesting_level) \
-                  #           + '\n' \
-                   #          + get_mspaces(nesting_level) + '}'
             value_right = ''
-
-        if record['STATE'] == 'CHILDREN':
-            pretty_children = get_children(record["VALUE"], nesting_level)
-        else:
-            pretty_children = ''
-
-        the_key = record["KEY"]
-
-        ############################################################################
-
-        if record['STATE'] == 'CHILDREN':
-            text_diff += f'{prefix}   {the_key}: ' \
-                         f'{pretty_children}'
-        elif record['STATE'] == 'ADDED':
-
-            if type(value_left) is dict:
-                txt = get_nicely_printed_dict({prefix + ' + ' + the_key: value_left}, nesting_level)
-            else:
-                txt = f' {the_key}: {value_left}'
-
-            #text_diff += f'{prefix} + {the_key}: {value_left}'
-            text_diff += txt
-
-        elif record['STATE'] == 'DELETED':
-            text_diff += f'{prefix} - {the_key}: {value_left}'
-        elif record['STATE'] == 'UNCHANGED':
-            text_diff += f'{prefix}   {the_key}: {value_left}'
-        elif record['STATE'] == 'CHANGED':
-            text_diff += f'{prefix} - {the_key}: ' \
-                         f'{value_left}'
-            text_diff += f'{prefix} + {the_key}: ' \
-                         f'{value_right}'
-
-    if nesting_level > 1:
-        text_diff += '\n' + get_spaces(nesting_level) + '}'
     else:
-        text_diff += '\n' + '}'
+        return record
+
+    the_key = record["KEY"]
+    key_value = f'{the_key}: {value_left}'
+
+    ##########################################
+
+    if record['STATE'] == 'CHILDREN':
+        text_diff += f'{prefix}   {key_value}'
+    elif record['STATE'] == 'ADDED':
+        text_diff += f'{prefix} + {key_value}'
+    elif record['STATE'] == 'DELETED':
+        text_diff += f'{prefix} - {key_value}'
+    elif record['STATE'] == 'UNCHANGED':
+        text_diff += f'{prefix}   {key_value}'
+    elif record['STATE'] == 'CHANGED':
+        text_diff += f'{prefix} - {key_value}'
+        text_diff += f'{prefix} + {the_key}: {value_right}'
+
+    #if nesting_level > 1:
+    text_diff += '\n' + get_mspaces(nesting_level) + '}'
+    # else:
+    #     text_diff += '\n' + '}'
 
     return text_diff
 
@@ -134,7 +111,9 @@ def get_children(data, nesting_level):
 def get_render_stylish(data):
 
     root_node = data.get('ROOT')
-    # kind of a guard expression to save some spaces on indentation-levels
+    # kind of a guard expression
+    # to save some spaces on indentation-levels
     if root_node is None:
         raise Exception('No ROOT node in the internal representation.')
-    return get_children(root_node, nesting_level=0)
+    #return get_children(root_node, nesting_level=0)
+    return render_data(root_node, nesting_level=0)
